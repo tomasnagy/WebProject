f46f6c1738d97fab22be77e07643410decf28d0d
Tomas Nagy <tomasngy@gmail.com>
2014-12-18 14:04:50 +0100

    section position calculation fixed, height bugged on safari(mac)

--- public/javascripts/guitarplayer/GuitarPlayerItem.js
+++ public/javascripts/guitarplayer/GuitarPlayerItem.js
@@ -50,25 +50,6 @@ var GuitarPlayerItem = function(selectedGuitar, isSupportGuitar) {
             ];
     }
 
-    //var manifest = [
-    //    {id:"cheapChord1", src:"CheapStudio/chord1CheapStudio.mp3"},
-    //    {id:"cheapChord2", src:"CheapStudio/chord2CheapStudio.mp3"},
-    //    {id:"cheapChord3", src:"CheapStudio/chord3CheapStudio.mp3"},
-    //    {id:"cheapChord4", src:"CheapStudio/chord4CheapStudio.mp3"},
-    //    {id:"heartBrokenChord1", src:"HeartBroken/chord1HeartBroken.mp3"},
-    //    {id:"heartBrokenChord2", src:"HeartBroken/chord2HeartBroken.mp3"},
-    //    {id:"heartBrokenChord3", src:"HeartBroken/chord3HeartBroken.mp3"},
-    //    {id:"heartBrokenChord4", src:"HeartBroken/chord4HeartBroken.mp3"},
-    //    {id:"practiceSpaceChord1", src:"PracticeSpace/chord1PracticeSpace.mp3"},
-    //    {id:"practiceSpaceChord2", src:"PracticeSpace/chord2PracticeSpace.mp3"},
-    //    {id:"practiceSpaceChord3", src:"PracticeSpace/chord3PracticeSpace.mp3"},
-    //    {id:"practiceSpaceChord4", src:"PracticeSpace/chord4PracticeSpace.mp3"},
-    //    {id:"woollyOctaveChord1", src:"WoollyOctave/chord1WoollyOctave.mp3"},
-    //    {id:"woollyOctaveChord2", src:"WoollyOctave/chord2WoollyOctave.mp3"},
-    //    {id:"woollyOctaveChord3", src:"WoollyOctave/chord3WoollyOctave.mp3"},
-    //    {id:"woollyOctaveChord4", src:"WoollyOctave/chord4WoollyOctave.mp3"}
-    //];
-
     createjs.Sound.alternateExtensions = ["mp3"];
     createjs.Sound.registerManifest(manifest, audioPath);
 };
@@ -115,7 +96,7 @@ GuitarPlayerItem.prototype = {
             this.chord3 = createjs.Sound.play(this.selectedGuitar + 'Chord3');
 
             if(this.isSupportGuitar) {
-                this.chord1.setVolume(0.5);
+                this.chord3.setVolume(0.5);
             }
 
         } else {
@@ -129,7 +110,7 @@ GuitarPlayerItem.prototype = {
             this.chord4 = createjs.Sound.play(this.selectedGuitar + 'Chord4');
 
             if(this.isSupportGuitar) {
-                this.chord1.setVolume(0.5);
+                this.chord4.setVolume(0.5);
             }
 
         } else {
