e89400d3f0cc3b55fa7c6fa4ec17ceeef8ba4ab2
Tomas Nagy <tomasngy@gmail.com>
2014-12-19 20:54:52 +0100

    opacity event added

--- public/javascripts/guitarhistory/animation/HistoryAnimationItem.js
+++ public/javascripts/guitarhistory/animation/HistoryAnimationItem.js
@@ -17,6 +17,7 @@ var HistoryAnimationItem = function(index, data) {
     this.tl2 = new TimelineMax({paused: 'true'});
     this.tl3 = new TimelineMax({paused: 'true'});
     this.tl4 = new TimelineMax({paused: 'true'});
+    this.tl5 = new TimelineMax({paused: 'true'});
     this.isOpen = false;
 };
 
@@ -32,7 +33,38 @@ HistoryAnimationItem.prototype = {
         }, false);
     },
     calculateAnimations: function() {
-        var self = this;
+        var self = this,
+            closeItemsEvent = new CustomEvent(
+                "closeItems",
+                {
+                    detail: {
+                        source: this.index
+                    },
+                    bubbles: true,
+                    cancelable: true
+                }
+            ),
+            fadeOutItemsEvent = new CustomEvent(
+                "fadeOutItems",
+                {
+                    detail: {
+                        source: this.index
+                    },
+                    bubbles: true,
+                    cancelable: true
+                }
+            ),
+            fadeInItemsEvent = new CustomEvent(
+                "fadeInItems",
+                {
+                    detail: {
+                        source: this.index
+                    },
+                    bubbles: true,
+                    cancelable: true
+                }
+            );
+
 
 
         this.fullHeight = parseInt(window.getComputedStyle(this.guitarItem, null).getPropertyValue('height'));
@@ -58,54 +90,78 @@ HistoryAnimationItem.prototype = {
         this.guitarItem.className = 'closedItem';
 
         // create animations
-        this.tl1.to(this.guitarItem, 0.4, {className: 'peekOpenItem'}).to(this.mainData, 0.2, {className: 'show'});
-        this.tl2.to(this.guitarItem, 0.4, {height: this.fullHeight}).to(this.extraData, 0, {className: 'block'}).to(this.extraData, 0.2, {className: 'fullshow'});
-        this.tl3.to(this.extraData, 0.2, {className: 'hidden'}).to(this.extraData, 0, {className: 'fullyhidden'}).to(this.guitarItem, 0.2, {height: this.smallHeight}).to(this.guitarItem, 0, {className: 'peekOpenItem'}).to(this.mainData, 0.2, {className: 'hidden'}).to(this.guitarItem, 0.5, {className: 'closedItem'});
-        this.tl4.to([this.mainData, this.extraData], 0.1, {className: 'hidden'}).to(this.guitarItem, 0.1, {className: 'hidden'}).to(this.extraData, 0, {className: 'fullyhidden'}).to(this.guitarItem,  0, {height: this.smallHeight});
+        this.tl1
+            .to(this.guitarItem, 0.4, {className: 'peekOpenItem'})
+            .to(this.mainData, 0.2, {className: 'show'});
 
-        // custom event
-        var event = new CustomEvent(
-            "closeItems",
-            {
-                detail: {
-                    source: this.index
-                },
-                bubbles: true,
-                cancelable: true
-            }
-        );
+        this.tl2
+            .to(this.guitarItem, 0.4, {height: this.fullHeight})
+            .to(this.extraData, 0, {className: 'block'})
+            .to(this.extraData, 0.2, {className: 'fullshow'});
+
+        this.tl3
+            .to(this.extraData, 0.2, {className: 'hidden'})
+            .to(this.extraData, 0, {className: 'fullyhidden'})
+            .to(this.guitarItem, 0.2, {height: this.smallHeight})
+            .to(this.guitarItem, 0, {className: 'peekOpenItem'})
+            .to(this.mainData, 0.2, {className: 'hidden'})
+            .to(this.guitarItem, 0.5, {className: 'closedItem'});
+
+        this.tl4
+            .to([this.mainData, this.extraData], 0.1, {className: 'hidden'})
+            .to(this.guitarItem, 0.1, {className: 'hidden'})
+            .to(this.extraData, 0, {className: 'fullyhidden'})
+            .to(this.guitarItem,  0, {height: this.smallHeight});
+
+        this.tl5.to(this.guitarItem, 0.4, {className: '+=opaqueItem'});
 
         // close current item
         document.addEventListener('closeItems', function(e) {
             if(e.detail.source !== self.index) {
                 if(self.isOpen) {
                     self.tl4.play(0);
-                    self.year.className = '';
+                    self.year.children[0].classList.remove('active');
                     self.isOpen = false;
                 }
             }
         }, false);
 
+        // fadeout current item
+        document.addEventListener('fadeOutItems', function(e) {
+            if(e.detail.source !== self.index && self.isOpen === true) {
+                self.tl5.play(0);
+            }
+        }, false);
+
+        // fadein current item
+        document.addEventListener('fadeInItems', function(e) {
+            if(e.detail.source !== self.index && self.isOpen === true) {
+                self.tl5.reverse();
+            }
+        }, false);
+
         // events
-        this.year.addEventListener('mouseenter', function () {
+        this.year.addEventListener('mouseenter', function (e) {
             if (!self.isOpen) {
                 self.tl1.play(0);
+                e.target.dispatchEvent(fadeOutItemsEvent);
             }
         });
 
-        this.year.addEventListener('mouseleave', function () {
+        this.year.addEventListener('mouseleave', function (e) {
             if (!self.isOpen) {
                 self.tl1.reverse();
+                e.target.dispatchEvent(fadeInItemsEvent);
             }
         });
 
 
         this.year.addEventListener('click', function (e) {
-            e.target.className = 'active';
+            e.target.classList.add('active');
             console.log(self.isOpen);
             self.isOpen = true;
             self.tl2.play(0);
-            e.target.dispatchEvent(event);
+            e.target.dispatchEvent(closeItemsEvent);
         });
 
     },
@@ -123,22 +179,27 @@ HistoryAnimationItem.prototype = {
         this.tl2.invalidate();
         this.tl3.invalidate();
         this.tl4.invalidate();
+        this.tl5.invalidate();
         this.tl1.pause(0, true);
         this.tl2.pause(0, true);
         this.tl3.pause(0, true);
         this.tl4.pause(0, true);
+        this.tl5.pause(0, true);
         this.tl1.kill();
         this.tl2.kill();
         this.tl3.kill();
         this.tl4.kill();
+        this.tl5.kill();
         this.tl1 = null;
         this.tl2 = null;
         this.tl3 = null;
         this.tl4 = null;
+        this.tl5 = null;
         this.tl1 = new TimelineMax({paused: 'true'});
         this.tl2 = new TimelineMax({paused: 'true'});
         this.tl3 = new TimelineMax({paused: 'true'});
         this.tl4 = new TimelineMax({paused: 'true'});
+        this.tl5 = new TimelineMax({paused: 'true'});
 
         this.fullHeight = 0;
         this.smallHeight = 0;
@@ -153,6 +214,7 @@ HistoryAnimationItem.prototype = {
         tempItem = this.year.cloneNode(true);
         this.year.parentNode.replaceChild(tempItem, this.year);
         this.year = tempItem;
+        this.year.children[0].classList.remove('active');
 
         for(i; i >=0; i--) {
             currentItem = this.guitarItem.children[i];
