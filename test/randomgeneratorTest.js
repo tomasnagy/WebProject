10ce50e71a27e67cfc6adb793b8700a0e729e0a2
Tomas Nagy <tomasngy@gmail.com>
2014-12-28 22:23:36 +0100

    tape + faucet added for tests + test 10000 unique id's

---
+++ test/randomgeneratorTest.js
@@ -0,0 +1,29 @@
+/**
+ * Created by tomasnagy on 28/12/14.
+ */
+var test = require('tape');
+var randomgenerator = require('../util/randomgenerator');
+
+test('100000 random ID\'s', function (t) {
+    var i = 99999,
+        allIDs = [];
+    t.plan(100000);
+    for(i; i >= 0; i--) {
+        randomgenerator.getRandomID(10, function(random) {
+            t.equal(allIDs.indexOf(random), -1, 'should be unique');
+            allIDs.push(random);
+        });
+    }
+});
+
+test('does a random contain only valid chars', function(t) {
+    randomgenerator.getRandomID(200, function(random) {
+        var validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+            i = random.length - 1;
+        console.log(i);
+        t.plan(200);
+        for(i; i >= 0; i--) {
+            t.ok(validChars.indexOf(random[i], 1), 'valid character');
+        }
+    });
+});
--- util/randomgenerator.js
+++ util/randomgenerator.js
@@ -8,6 +8,8 @@ module.exports = (function() {
             pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
             l = pool.length;
 
+        length--;
+
         for(length; length >= 0; length--) {
             result += pool.substr(Math.floor(Math.random() * l), 1);
         }
